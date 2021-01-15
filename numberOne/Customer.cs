17755d8e9be1d0c3f3ab93d856aa29517adae3f5
AneesUr Rehman <AneesUr.Rehman001@umb.edu>
2021-01-15 07:10:58 -0500

    initial

---
+++ numberOne/Customer.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace numberOne
+{
+    class Customer
+    {
+        private int id;
+        private string name;
+
+        public Customer(int id, string name)
+        {
+            this.id = id;
+            this.name = name;
+        }
+
+        public string customerName(int id)
+        {
+            return this.name;
+        }
+
+        public int customerId(string name)
+        {
+            return this.id;
+        }
+
+
+    }
+}
---
+++ numberOne/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace numberOne
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer one = new Customer(1, "Anees");
+            Customer two = new Customer(2, "Gabriel");
+            Customer three = new Customer(3, "Matthew");
+
+            Console.WriteLine(customerId(1));
+            
+        }
+    }
+}
